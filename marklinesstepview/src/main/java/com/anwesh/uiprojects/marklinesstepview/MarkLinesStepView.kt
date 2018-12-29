831d3043c2d9a65b8c6358c310ac2633a446e63b
Anwesh43 <anweshthecool0@gmail.com>
2018-12-29 12:36:58 +0530

    adding MLSNode class

--- marklinesstepview/src/main/java/com/anwesh/uiprojects/marklinesstepview/MarkLinesStepView.kt
+++ marklinesstepview/src/main/java/com/anwesh/uiprojects/marklinesstepview/MarkLinesStepView.kt
@@ -124,4 +124,49 @@ class MarkLinesStepView(ctx : Context) : View(ctx)  {
             }
         }
     }
+
+    data class MLSNode(var i : Int, val state : State = State()) {
+
+        private var next : MLSNode? = null
+
+        private var prev : MLSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = MLSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawMLSNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : MLSNode {
+            var curr : MLSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this 
+        }
+    }
 }
