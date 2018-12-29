b15156fb93bec15f8823bb739fb98306c36ab373
Anwesh43 <anweshthecool0@gmail.com>
2018-12-29 12:42:13 +0530

    using rendererr in view

--- marklinesstepview/src/main/java/com/anwesh/uiprojects/marklinesstepview/MarkLinesStepView.kt
+++ marklinesstepview/src/main/java/com/anwesh/uiprojects/marklinesstepview/MarkLinesStepView.kt
@@ -63,15 +63,16 @@ fun Canvas.drawMLSNode(i : Int, scale : Float, paint : Paint) {
 class MarkLinesStepView(ctx : Context) : View(ctx)  {
 
     private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val renderer : Renderer = Renderer(this)
 
     override fun onDraw(canvas : Canvas) {
-
+        renderer.render(canvas, paint)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
