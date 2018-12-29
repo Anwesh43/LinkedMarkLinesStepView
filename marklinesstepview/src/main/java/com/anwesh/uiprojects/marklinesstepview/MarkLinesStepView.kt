88fadd78418bce0f918bf35a7f4ab5b1dc88a9cd
Anwesh43 <anweshthecool0@gmail.com>
2018-12-29 12:33:52 +0530

    adding Animator class

--- marklinesstepview/src/main/java/com/anwesh/uiprojects/marklinesstepview/MarkLinesStepView.kt
+++ marklinesstepview/src/main/java/com/anwesh/uiprojects/marklinesstepview/MarkLinesStepView.kt
@@ -96,4 +96,32 @@ class MarkLinesStepView(ctx : Context) : View(ctx)  {
             }
         }
     }
+
+    data class Animator(var view : View, var animated : Boolean = false) {
+
+        fun animate(cb : () -> Unit) {
+            if (animated) {
+                cb()
+                try {
+                    Thread.sleep(DELAY)
+                    view.invalidate()
+                } catch(ex : Exception) {
+
+                }
+            }
+        }
+
+        fun start() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+
+        fun stop() {
+            if (animated) {
+                animated = false
+            }
+        }
+    }
 }
