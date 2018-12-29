5f6ee2e3a9023be3f46b5db706a020357879c5eb
Anwesh43 <anweshthecool0@gmail.com>
2018-12-29 12:28:52 +0530

    adding drawMLSNode canvas extension method

--- marklinesstepview/src/main/java/com/anwesh/uiprojects/marklinesstepview/MarkLinesStepView.kt
+++ marklinesstepview/src/main/java/com/anwesh/uiprojects/marklinesstepview/MarkLinesStepView.kt
@@ -33,3 +33,29 @@ fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
 fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.inverse() + scaleFactor() * b.inverse()
 
 fun Float.updateScale(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
+
+fun Canvas.drawMLSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    val xGap : Float = size / ((lines + 1)/2)
+    paint.color = foreColor
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND 
+    save()
+    translate(gap * (i + 1), h/2)
+    rotate(-90f * sc1)
+    translate(-size, -size)
+    drawLine(0f, 0f, 0f, 2 * size, paint)
+    for (j in 0..(lines - 1)) {
+        var newJ : Int = j + 1
+        newJ = Math.min(lines - newJ, newJ)
+        val sc : Float = sc1.divideScale(j, lines)
+        val x : Float = newJ * xGap * sc
+        drawLine(0f, 0f, x, 0f, paint)
+    }
+    restore()
+}
