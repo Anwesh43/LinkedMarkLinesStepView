581ebfa3e2dec17b8814854219076b8fd40e08b9
Anwesh43 <anweshthecool0@gmail.com>
2018-12-29 12:12:18 +0530

    adding constants and importing libraries

--- marklinesstepview/src/main/java/com/anwesh/uiprojects/marklinesstepview/MarkLinesStepView.kt
+++ marklinesstepview/src/main/java/com/anwesh/uiprojects/marklinesstepview/MarkLinesStepView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.marklinesstepview
 /**
  * Created by anweshmishra on 29/12/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+
+val nodes : Int = 5
+val lines : Int = 5
+val foreColor : Int = Color.parseColor("#4CAF50")
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val backColor : Int = Color.parseColor("#212121")
+val DELAY : Long = 25
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.6f
