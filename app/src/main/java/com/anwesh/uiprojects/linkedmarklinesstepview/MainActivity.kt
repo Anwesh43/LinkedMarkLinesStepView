439894feae1205e95e82b36d515beb30d89deeed
Anwesh43 <anweshthecool0@gmail.com>
2018-12-29 12:46:11 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedmarklinesstepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedmarklinesstepview/MainActivity.kt
@@ -2,11 +2,11 @@ package com.anwesh.uiprojects.linkedmarklinesstepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-
+import com.anwesh.uiprojects.marklinesstepview.MarkLinesStepView
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val view : MarkLinesStepView = MarkLinesStepView.create(this)
     }
 }
