4a71b60cf5be32f89409970d11d08b4fb5cc3d29
Anwesh43 <anweshthecool0@gmail.com>
2018-12-29 12:18:19 +0530

    adding Float and Int extension methods

--- marklinesstepview/src/main/java/com/anwesh/uiprojects/marklinesstepview/MarkLinesStepView.kt
+++ marklinesstepview/src/main/java/com/anwesh/uiprojects/marklinesstepview/MarkLinesStepView.kt
@@ -21,3 +21,15 @@ val backColor : Int = Color.parseColor("#212121")
 val DELAY : Long = 25
 val strokeFactor : Int = 90
 val sizeFactor : Float = 2.6f
+
+fun Int.inverse() : Float = 1f / this
+
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i / n)
+
+fun Float.divideScale(i : Int, n : Int) = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
+
+fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.inverse() + scaleFactor() * b.inverse()
+
+fun Float.updateScale(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
