c4c957d04ec63df62bbc80db1488182be989eb89
Anwesh43 <anweshthecool0@gmail.com>
2018-12-29 12:47:06 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedmarklinesstepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedmarklinesstepview/MainActivity.kt
@@ -2,11 +2,18 @@ package com.anwesh.uiprojects.linkedmarklinesstepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.marklinesstepview.MarkLinesStepView
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val view : MarkLinesStepView = MarkLinesStepView.create(this)
+        fullScreen()
+    }
+
+    fun MainActivity.fullScreen() {
+        supportActionBar?.hide()
+        window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
