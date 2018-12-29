42582c62f938b9071a7afb6720b485418319d387
Anwesh43 <anweshthecool0@gmail.com>
2018-12-29 12:39:33 +0530

    adding MarkLinesStep class

--- marklinesstepview/src/main/java/com/anwesh/uiprojects/marklinesstepview/MarkLinesStepView.kt
+++ marklinesstepview/src/main/java/com/anwesh/uiprojects/marklinesstepview/MarkLinesStepView.kt
@@ -166,7 +166,31 @@ class MarkLinesStepView(ctx : Context) : View(ctx)  {
                 return curr
             }
             cb()
-            return this 
+            return this
+        }
+    }
+
+    data class MarkLinesStep(var i : Int) {
+
+        private val root : MLSNode = MLSNode(0)
+        private var curr : MLSNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
         }
     }
 }
